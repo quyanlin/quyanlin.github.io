ad8baa476131c622faf5708deb6aa00c331ed67d
quyanlin <56414428+quyanlin@users.noreply.github.com>
2024-11-02 21:51:29 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,14 +15,6 @@
     });
   }
 
-  //preloader
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
-
   // Smooth scroll for the navigation menu and links with .scrollto classes
   $(document).on('click', '.nav-menu a, .scrollto', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
